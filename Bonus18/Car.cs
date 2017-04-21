26b0a8f50d8ce61f8756a6133c445f57c8af0118
Charles Lambrecht <lambrechtca@gmail.com>
2017-04-21 14:24:19 -0400

    Initial commit

---
+++ Bonus18/Car.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class Car
+    {
+        //variables
+        private string CarMake;
+        private string CarModel;
+        private int CarYear;
+        private double CarPrice;
+
+        public string Make
+        {
+            get
+            {
+                return CarMake;
+            }
+
+            set
+            {
+                CarMake = value;
+            }
+        }
+
+        public string Model
+        {
+            get
+            {
+                return CarModel;
+            }
+
+            set
+            {
+                CarModel = value;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return CarYear;
+            }
+
+            set
+            {
+                CarYear = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return CarPrice;
+            }
+
+            set
+            {
+                CarPrice = value;
+            }
+        }
+
+        //properties
+
+
+        //constructors
+        public Car()
+        {
+            CarMake = "Honda";
+            CarModel = "Civic";
+            CarYear = 2000;
+            CarPrice = 5000.00;
+        }
+
+        public Car(string MakeInput, string ModelInput, int YearInput, double PriceInput)
+        {
+            CarMake = MakeInput;
+            CarModel = ModelInput;
+            CarYear = YearInput;
+            CarPrice = PriceInput;
+        }
+    }
+}
---
+++ Bonus18/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many cars would you like to enter data for?");
+            int numCars = int.Parse(Console.ReadLine());
+
+            List<object> CarList = new List<object>();
+
+            for (int i = 0; i < numCars; i++)
+            {
+                Console.WriteLine("Would you like to enter data for Car {0}? (y = yes/n = default)", i + 1);
+                string defaultCar = Console.ReadLine().ToLower();
+
+                if (defaultCar == "y")
+                {
+                    Car Car_i = new Car();
+                    CarList[i] = Car_i;
+                }
+                else if (defaultCar == "n")
+                {
+                    Car Car_i = new Car();
+                    CarList[i] = Car_i;
+                }
+                else
+                {
+
+                }
+
+            }
+        }
+    }
+}
